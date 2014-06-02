af44e4c1b94dcc28e195c6785a497efb74894d2b
Oleg Stasula <oleg.s@treelineinteractive.com>
2014-06-02 12:06:49 +0300

    v.0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='keepboo-bookmarksparser',
-    version='0.0.1',
+    version='0.1.2',
     packages=['keepoo_bookmarksparser'],
     url='https://github.com/KEEPBOO/bookmarks-parser',
     license='',
